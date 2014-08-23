7949690ce555bdbcaa3791fbc0064d7543df19f6
David Javate <davidjavate@David-Javates-MacBook-Pro-2.local>
2014-08-23 15:44:32 -0700

    trying to merge with blessed repo" "

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+	def index
+		@users=User.all
+		@posts= Post.all
+	end
+
+	def new
+		@user = User.new 
+	end
+
+	def create
+		new_user = params[:user].permit[:first_name, :last_name, :email, :image_url]
+		user= User.create(new_user)
+		redirect_to "/"
+	end
+
+#test
+end
