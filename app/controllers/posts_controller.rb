a18942621abbb47906254d2f553c124d000018c3
Randall Leung <leungran@gmail.com>
2014-08-23 17:00:45 -0700

    Working Post#New

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,7 +11,20 @@ class PostsController < ApplicationController
 
   def create
     find_user_id
-     
+    new_post = params[:post].permit(:title, :body)
+    @post = find_user_id.posts.create(new_post)
+
+    new_tag = params[:tags].split(",").map(&:strip).map(&:downcase)
+    new_tag.each do |tag_str|
+      tag = Tag.find_by_name(tag_str)
+      if tag == nil
+        tag = Tag.create(:name => tag_str)
+      end
+      # Need to fix this part?  A tag belongs to many Posts?  Still working
+      # new_post.tags << tag
+    end
+
+    redirect_to find_user_id
   end
 
 
