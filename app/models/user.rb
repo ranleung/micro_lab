3b8d5bda9390462f57ab70d2ef297ab8a3b5bd59
Dustin Yu <dustin520@gmail.com>
2014-08-25 01:21:13 -0700

    added site nav for pages and users views

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,8 @@ class User < ActiveRecord::Base
 			:presence => true,
 			:uniqueness => true
 
-		validates_confirmation_of :email
-	  validates_presence_of :email_confirmation
+		# validates_confirmation_of :email
+	 #  validates_presence_of :email_confirmation
 end
 
 
