b3129b208c729b07cd729dc21688d27db91279c6
Dustin Yu <dustin520@gmail.com>
2014-08-25 18:19:05 -0700

    readded blessed repo

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,21 +1,26 @@
 class SessionController < ApplicationController
   def new
-  	#Log In
+    #Log In
   end
 
   def create
-  	#where authentication happens
-  	@user = User.authenticate(params[:user][:email],params[:user][:password])
+    #where authentication happens
+    @user = User.authenticate(params[:user][:email],params[:user][:password])
 
-  	if @user
-  		session[:user_id] = @user.id
-  		redirect_to root_path
-  	else
-  		redirect_to login_path
-  	end
+    if @user
+      session[:user_id] = @user.id
+      p "LOGGED IN"
+      redirect_to root_path
+    else
+      p "DID NOT LOG IN"
+      redirect_to login_path
+    end
 
   end
 
   def destroy
+    session[:user_id] = nil
+    p "SESSION DESTROYED - LOGGED OUT"
+    redirect_to login_path
   end
 end
