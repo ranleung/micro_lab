19e24b1f474363874e1f3275478b3e34b379bbe1
Randall Leung <leungran@gmail.com>
2014-08-26 18:38:35 -0700

    added request for new pw working

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -5,20 +5,31 @@ class PasswordsController < ApplicationController
 
   def create
   	user = User.find_by_email(params[:email])
-  	p "The email to reset password is: #{user}"
 
   	if user
   		user.set_password_reset
   		UserMailer.password_reset(user).deliver
-  		redirect_to '/', notice: "Email was sent with instructions"
+	  	p "The email to reset password is: #{user}"
+  		redirect_to '/' # , notice: "Email was sent with instructions"
   	else
   		redirect_to action: 'new', :notice => "Invalid Email"
 	  end
 	end
 
 	def edit
-		
+		@user = User.find_by_code(params[:id])
+		@password = User.find_by_code(params[:id])
 	end
 
+	def update		
+		@user = User.find_by_code(params[:id])
+
+		if @user
+      update_password = params.require(:user).permit(:password)
+      @user.update_attributes(:password => update_password[:password])
+			p "PASSWORD UPDATED"
+			redirect_to '/'
+		end
+	end
 
 end
