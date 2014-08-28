f1dd5634ea1d1ff917a698d95d3af9ae6bcfb967
Randall Leung <leungran@gmail.com>
2014-08-27 17:10:12 -0700

    added user images

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 
 # Creating Users
 
-dustin = User.create({email: "dustin@gmail.com", first_name: "Dustin", last_name: "Yu", image_url: "www.yahoo.com", password: "123"})
-david = User.create({email: "david@hotmail.com", first_name: "David", last_name: "Javate", image_url: "www.google.com",password: "123"})
-randall = User.create({email: "randall@yahoo.com", first_name: "Randall", last_name: "Leung", image_url: "www.facebook.com",password: "123"})
+dustin = User.create({email: "dustin@gmail.com", first_name: "Dustin", last_name: "Yu", image_url: "https://justplanetosh.files.wordpress.com/2013/02/dustin-hoffman-hook.jpg", password: "123"})
+david = User.create({email: "david@hotmail.com", first_name: "David", last_name: "Javate", image_url: "http://en.africatopsports.com/wp-content/uploads/2014/03/david-beckham-Photos.jpg",password: "123"})
+randall = User.create({email: "randall@yahoo.com", first_name: "Randall", last_name: "Leung", image_url: "http://img2.wikia.nocookie.net/__cb20130204173111/recess/images/4/47/Randall_Great.jpg",password: "123"})
 
 
 # Creating Pages
@@ -42,10 +42,10 @@ post1.comments << comment1
 post1.comments << comment2
 
 # Test to see if Posts and Tags have many-to-many
-post1.tags.create({name: "Awesome"})
-post1.tags.create({name: "COOL"})
-post2.tags.create({name: "WOW"})
-post2.tags.create({name: "NICE"})
+post1.tags.create({name: "awesome"})
+post1.tags.create({name: "cool"})
+post2.tags.create({name: "wow"})
+post2.tags.create({name: "nice"})
 
 
 
